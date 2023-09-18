f581a211563d6720e5ad1311c81697231c011965
Voosk <milkgolium@gmail.com>
2023-09-19 00:03:20 +0800

    update

--- docs/.vuepress/.temp/internal/pagesRoutes.js
+++ docs/.vuepress/.temp/internal/pagesRoutes.js
@@ -1,8 +1,8 @@
 export const pagesRoutes = [
   ["v-3706649a","/404.html",{"title":""},[]],
-  ["v-8daa1a0e","/",{"title":""},["/README.md"]],
+  ["v-8daa1a0e","/",{"title":"前言"},["/README.md"]],
   ["v-6ce48554","/contributing.html",{"title":"helloa"},[":md"]],
   ["v-fb0f0066","/guide/getting-started.html",{"title":"ok a!"},[":md"]],
   ["v-fffb8e28","/guide/",{"title":"test!!"},["/guide/README.md"]],
-  ["v-8daa1a0e","/",{"title":"古生物百科前言"},["/index.md"]],
+  ["v-8daa1a0e","/",{"title":"前言"},["/index.md"]],
 ]
--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,4 +1,4 @@
-export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1695051837000,\"contributors\":[{\"name\":\"Ensterin Albert\",\"email\":\"milkgolium@gmail.com\",\"commits\":1},{\"name\":\"Voosk\",\"email\":\"milkgolium@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"前言\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"什么是古生物？\",\"slug\":\"什么是古生物\",\"link\":\"#什么是古生物\",\"children\":[]},{\"level\":2,\"title\":\"什么是化石？\",\"slug\":\"什么是化石\",\"link\":\"#什么是化石\",\"children\":[]},{\"level\":2,\"title\":\"什么是活化石？\",\"slug\":\"什么是活化石\",\"link\":\"#什么是活化石\",\"children\":[]},{\"level\":2,\"title\":\"研究古生物有什么意义？\",\"slug\":\"研究古生物有什么意义\",\"link\":\"#研究古生物有什么意义\",\"children\":[]}],\"git\":{\"updatedTime\":1695052741000,\"contributors\":[{\"name\":\"Voosk\",\"email\":\"milkgolium@gmail.com\",\"commits\":2}]},\"filePathRelative\":\"index.md\"}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
